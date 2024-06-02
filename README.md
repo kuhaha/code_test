901a64fa624601859527de6f10c9724f73461294
kuhaha <kevin.yr.cheng@gmail.com>
2024-06-02 09:57:59 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# 書誌情報を検索できるサービス・APIs
+
+## 出版書誌データベース
+
+- https://www.books.or.jp
+
+『出版書誌データベース』、通称『Books』は出版業界唯一の出版情報の公開データベースです。紙の書籍、電子書籍、定期刊行物など数多くの出版情報を検索できます。日本書籍出版協会から、日本出版インフラセンターへ管理・運営を移行しています。
+
+一般社団法人 日本出版インフラセンター（JPO）
+- 出版情報登録センター(JPRO)
+- 出版書誌データベース（Books）
+- 担当： 浜崎・米津
+- Email： info-books@jpo-center.jp
+
+## 国会図書館
+
+- https://www.ndl.go.jp/
+- https://ndlsearch.ndl.go.jp/ 
+- https://www.ndl.go.jp/jp/use/api/
+  - 検索API（SRU,OpeSearch,OpenURL,OAI-PMH） 
+  - 書影API (https://ndlsearch.ndl.go.jp/thumbnail/[isbn].jpg)
+
+
+## Google Books
+
+- https://books.google.co.jp/
+
+書籍の全文が登録された世界最大級の包括的なインデックスを検索できます。
+
+### Google Books APIs
+- https://developers.google.com/books?hl=ja
+- https://labo.kon-ruri.co.jp/google-books-apis/
+
+著者名、書籍タイトル、ISBNなどで書籍の情報を得るには、Google Books APIsがかなり手頃で使いやすいです。
+
+- 特別な認証は必要なし
+- レスポンスまでの速度も早い
+
+とあって、使いやすさ抜群。一方で、書籍の網羅率には国会図書館サーチなどにはやや劣りますが、ISBN10でもISBN13でも検索ができるので、「書籍情報をちょっと調べたい」というときには最適です。
---
+++ googlebooks.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <title>Google Books APIs</title>
+</head>
+<body>
+<?php 
+  include 'googlebooksapi.php'; 
+  function get($obj, $property, $default='N/A')
+  {
+    if ($obj instanceof stdClass and property_exists($obj, $property))
+      return $obj->$property;
+    return $default;
+  }
+?>
+<p>全<?=$total_count?>件中、<?=$get_count?>件を表示中</p>
+<!-- 1件以上取得した書籍情報がある場合 -->
+<?php if($get_count > 0): ?>
+  <div class="loop_books">
+    <!-- 取得した書籍情報を順に表示 -->
+    <?php foreach($books as $book):
+        $info = get($book, 'volumeInfo');
+        // タイトル
+        $title = get($info, 'title');
+        // 出版社
+        $publisher = get($info, 'publisher');
+        $publishedDate = get($info, 'publishedDate');
+        $isbn = get($info, 'industryIdentifiers');
+        $isbn10 = $isbn[0]->type=='ISBN_10' ? get($isbn[0],'identifier') : 'N/A';
+        $isbn13 = $isbn[1]->type=='ISBN_13' ? get($isbn[1],'identifier') : 'N/A';
+        // サムネ画像
+        $imglink = get($info,'imageLinks');
+        $thumbnail = get($imglink,'thumbnail');
+        // 著者（配列なのでカンマ区切りに変更）
+        $authors = get($info, 'authors');
+        if (is_array($authors))
+          $authors = implode(',', $authors);
+
+        $intro = get(get($book, 'searchInfo'), 'textSnippet');
+    ?>
+      <div class="loop_books_item">
+        <img src="<?=$thumbnail?>" alt="<?=$title?>"><br />
+        <p>
+          <b><?=$title?></b><br />
+          著　者：<?=$authors?><br />
+          出版社：<?=$publisher?><br />
+          出版日：<?=$publishedDate?><br />
+          ISBN13：<?=$isbn10?><br />
+          概要：<?=$intro?><br />
+        </p>
+      </div>
+    <?php endforeach?>
+
+  </div><!-- ./loop_books -->
+
+<!-- 書籍情報が取得されていない場合 -->
+<?php else: ?>
+  <p>情報が有りません</p>
+
+<?php endif?>
+
+<?php
+echo '<pre>';
+var_dump($books);
+echo '</pre>';
+?>
+
+</body>
+</html>
